87cb800e644dd4b4f8e24e34c6847a3263dcdb16
Ron <ron@ronsbrain.com>
2022-12-03 10:14:39 -0500

    Adds comments for day 3

--- 3-1.rs
+++ 3-1.rs
@@ -7,13 +7,15 @@ fn main() {
     let mut total = 0;
     for line in lines {
         let unwrapped = line.unwrap();
+        /* Need to store this temporarily in order to reference it later */
         let bytes = unwrapped.as_bytes();
+        /* Clone the iterators so the HashSet has values and not references */
         let left: HashSet<u8> = HashSet::from_iter(bytes[..bytes.len() / 2].iter().cloned());
         let right: HashSet<u8> = HashSet::from_iter(bytes[bytes.len() / 2..].iter().cloned());
         let unique = left.intersection(&right).next().unwrap();
         total = total + match *unique as char {
-            'a'..='z' => (unique - 'a' as u8 + 1) as i32,
-            'A'..='Z' => (unique - 'A' as u8 + 27) as i32,
+            'a'..='z' => (unique - 'a' as u8 + 1) as i32, // Start priority at 1
+            'A'..='Z' => (unique - 'A' as u8 + 27) as i32, // Start priortiy as 27
             _ => panic!("You done goofed: {}", unique),
         };
     }
--- 3-2.rs
+++ 3-2.rs
@@ -5,17 +5,21 @@ use std::iter::FromIterator;
 fn main() {
     let mut lines = io::stdin().lines();
     let mut total = 0;
+    /* Gather three lines at a time. Neat trick. */
     while let Some(chunk) = Some([lines.next(), lines.next(), lines.next()]) {
         if !chunk[0].is_some() {
+            /* No more lines. We're done. */
             break;
         }
         let mut remaining = HashSet::new();
         for line in chunk {
             let unwrapped = line.unwrap();
+            /* Need to store this so it stays in scope. */
             let temp = unwrapped.expect("Didn't get lines?");
             let bytes = temp.as_bytes();
             let sack: HashSet<u8> = HashSet::from_iter(bytes.iter().cloned());
             if remaining.len() == 0 {
+                /* We've not looked at any rucksacks yet, this is the initial */
                 remaining.extend(&sack);
             } else {
                 remaining = HashSet::from_iter(remaining.intersection(&sack).cloned())
@@ -23,8 +27,8 @@ fn main() {
         }
         let unique = remaining.iter().next().unwrap();
         total = total + match *unique as char {
-            'a'..='z' => (unique - 'a' as u8 + 1) as i32,
-            'A'..='Z' => (unique - 'A' as u8 + 27) as i32,
+            'a'..='z' => (unique - 'a' as u8 + 1) as i32, // Start priority at 1
+            'A'..='Z' => (unique - 'A' as u8 + 27) as i32, // Start priority at 27
             _ => panic!("You done goofed: {}", unique),
         };
     }
